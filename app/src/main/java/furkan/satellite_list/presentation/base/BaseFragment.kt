73be065f79f82ece77f0cf00bfc647eaf38e5ad4
Furkan Kaymaz <furkannkaymaz@gmail.com>
2023-02-17 21:03:42 +0300

    presentation base classes added

---
+++ app/src/main/java/furkan/satellite_list/presentation/base/BaseFragment.kt
@@ -0,0 +1,38 @@
+package furkan.satellite_list.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding, VM : ViewModel> : Fragment() {
+
+    private var _binding: VB? = null
+    val binding get() = _binding
+
+    abstract val viewModel: VM
+
+    open fun observerData() {}
+    open fun clickListeners() {}
+
+    abstract fun viewCreated()
+
+    abstract fun layoutResource(inflater: LayoutInflater, container: ViewGroup?): VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = layoutResource(inflater, container)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/furkan/satellite_list/presentation/base/BaseViewHolder.kt
@@ -0,0 +1,9 @@
+package furkan.satellite_list.presentation.base
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+open class BaseViewHolder<D>(bindingBase : ViewBinding): RecyclerView.ViewHolder(bindingBase.root) {
+    open fun bind(data: D, onItemClick: ((D) -> Unit)? = null) {}
+}
+
